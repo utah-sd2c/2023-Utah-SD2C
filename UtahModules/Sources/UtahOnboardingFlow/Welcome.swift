27293a01ab977a7df91021cd56ffb2e72f96a91f
Audrey Lin <84480051+ivyalin2015@users.noreply.github.com>
2023-02-26 14:24:04 -0800

    Onboarding flow pgs 1,2 (#52)

--- UtahModules/Sources/UtahOnboardingFlow/Welcome.swift
+++ UtahModules/Sources/UtahOnboardingFlow/Welcome.swift
@@ -17,21 +17,21 @@ struct Welcome: View {
     
     var body: some View {
         OnboardingView(
-            title: "Welcome to VascuTrack".moduleLocalized,
+            title: "Welcome to U-STEP".moduleLocalized,
             subtitle: "A collaboration between Stanford University & University of Utah".moduleLocalized,
             areas: [
                 .init(
                     icon: Image(systemName: "apps.iphone"),
                     title: "Track your health".moduleLocalized,
-                    description: "Easily see your biometric data".moduleLocalized
+                    description: "Easily see your health data".moduleLocalized
                 ),
                 .init(
-                    icon: Image(systemName: "shippingbox.fill"),
-                    title: "Report your outcomes".moduleLocalized,
+                    icon: Image(systemName: "figure.walk"),
+                    title: "Report your health aoutcomes".moduleLocalized,
                     description: "Fill out surveys about your health".moduleLocalized
                 ),
                 .init(
-                    icon: Image(systemName: "list.bullet.clipboard.fill"),
+                    icon: Image(systemName: "cross.circle.fill"),
                     title: "Streamline your care".moduleLocalized,
                     description: "Help your doctors help you get better".moduleLocalized
                 )
