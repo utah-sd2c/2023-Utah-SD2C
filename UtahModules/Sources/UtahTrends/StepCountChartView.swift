bed51d0873cd84ced6f47cbcd47e2896157d6ccc
Jennifer Xu <56773304+jennxu23@users.noreply.github.com>
2023-03-15 22:16:02 -0700

    Jenn/stepcount chart (#116)

--- UtahModules/Sources/UtahTrends/StepCountChartView.swift
+++ UtahModules/Sources/UtahTrends/StepCountChartView.swift
@@ -8,9 +8,11 @@
 
 
 import Charts
+import FHIR
+import FirebaseFirestore
 import SwiftUI
+import UtahSharedContext
 
-// Step count
 struct StepCount: Identifiable {
     let date: String
     let count: Int
@@ -21,6 +23,7 @@ struct StepCount: Identifiable {
 func date_formatter(date: Date) -> String {
     let dateFormatter = DateFormatter()
     dateFormatter.locale = Locale(identifier: "en_US")
+    dateFormatter.dateFormat = "M/dd"
     let formatted = dateFormatter.string(from: date)
     return formatted
 }
@@ -37,13 +40,16 @@ var stepDummyData: [StepCount] = [
 
 
 struct StepCountChart: View {
+    @EnvironmentObject var firestoreManager: FirestoreManager
+    @State var chartData: [StepCount] = []
+
     var body: some View {
         VStack(alignment: .leading) {
             VStack(alignment: .center) {
                 Text("Step Count")
                     .font(.headline)
                 Chart {
-                    ForEach(stepDummyData) { datum in
+                    ForEach(chartData) { datum in
                         BarMark(
                             x: .value("Date", datum.date),
                             y: .value("Step Count", datum.count)
@@ -58,8 +64,47 @@ struct StepCountChart: View {
                 .foregroundColor(Color(.systemBackground))
                 .shadow(radius: 5)
         }
+        .task {
+            group(firestoreManager.observations)
+        }
     }
+    
+    func group(_ data: [(date: Date, value: Double)]) {
+            let calendar = Calendar.current
+            let startOfDay = calendar.startOfDay(for: Date())
+        var latestDate: Date = calendar.date(byAdding: .day, value: -6, to: startOfDay) ?? Date()
+            var filteredData: [(Date, Double)] = []
+            
+            Calendar.current.enumerateDates(
+                startingAfter: latestDate,
+                matching: DateComponents(hour: 0),
+                matchingPolicy: .nextTime
+            ) { result, _, stop in
+                guard let result else {
+                    stop = true
+                    return
+                }
+                
+                let summedUpData = data
+                    .filter { element in
+                        latestDate < element.date && element.date < result
+                    }
+                    .map {
+                        $0.value
+                    }
+                    .reduce(0.0, +)
+                filteredData.append((result, summedUpData))
+                
+                latestDate = result
+                
+                if result > Date() {
+                    stop = true
+                }
+            }
+        self.chartData = filteredData.map { .init(date: date_formatter(date: $0.0), count: Int($0.1)) } as [StepCount]
+        }
 }
+
 
 struct StepCountChart_Previews: PreviewProvider {
     static var previews: some View {
